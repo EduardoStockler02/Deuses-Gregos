e61b16ae0e15d2c0ce4baddcc0c0e15ed65e4db9
Eduardo Stockler <stocklerdudu@gmail.com>
2025-02-18 23:24:05 -0300

    consegui alguma coisa

--- src/main.js
+++ src/main.js
@@ -1,9 +1,24 @@
-let index = 1;
+let index = 0;
+const slides = document.querySelectorAll('.carousel .item');
+const totalSlides = slides.length;
+
+
 function moveSlide(direction) {
     const slides = document.querySelectorAll('.carousel img');
     const totalSlides = slides.length
+
     slides[index].classList.remove('active');
     index = (index + direction + totalSlides) % totalSlides;
     slides[index].classList.add('active');
 }
 
+slides[index].classList.add('active');
+
+document.addEventListener('keydown', (event)=>{
+    if (event.key === 'ArrowRight') {
+        moveSlide(1);
+    }
+    else if(event.key === 'ArrowLeft') {
+        moveSlide(-1);
+    }
+});
