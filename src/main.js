2515797ba11da9af7eff467da4c686caa78c8029
imLeonam <leonammrib@gmail.com>
2025-02-19 10:25:53 -0300

    style changes for items and transitions

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 let index = 0;
 const slides = document.querySelectorAll('.carousel .item');
 const totalSlides = slides.length;
-const items = document.querySelectorAll('.carousel .item');
 
 function moveSlide(direction) {
-    const slides = document.querySelectorAll('.carousel img');
+    const slides = document.querySelectorAll('.carousel .item');
     const totalSlides = slides.length
 
     slides[index].classList.remove('active');
@@ -12,21 +11,11 @@ function moveSlide(direction) {
     slides[index].classList.add('active');
 }
 
-slides[index].classList.add('active');
-
-document.addEventListener('keydown', (event)=>{
+document.addEventListener('keydown', (event) => {
     if (event.key === 'ArrowRight') {
         moveSlide(1);
     }
-    else if(event.key === 'ArrowLeft') {
+    else if (event.key === 'ArrowLeft') {
         moveSlide(-1);
     }
-});
-
-document.array.forEach(element => {
-    document.addEventListener("click", () => {
-        items.forEach((el) => classList.remove("active"));
-
-        items.classList.add("active");
-    })
 });
